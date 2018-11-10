d7bd5657e1521c955898561101626ecc15ab5f4a
Chuck Findlay <chuck@findlayis.me>
2018-11-10 02:07:20 -0700

    Corrected license header on source files (should be LGPL)

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
     // Copyright 2018 Chuck Findlay
-    // This software is licensed under the GNU General Public License v3.0
+    // This software is licensed under the GNU Lesser General Public License v3.0
 
     // Our session bit needs to be in here first. It'll kick out anyone who is not logged in
     include('../session.php');
--- www/login.php
+++ www/login.php
@@ -1,6 +1,6 @@
 <?php
     // Copyright 2018 Chuck Findlay
-    // This software is licensed under the GNU General Public License v3.0
+    // This software is licensed under the GNU Lesser General Public License v3.0
 
     include('../config.inc.php');
 

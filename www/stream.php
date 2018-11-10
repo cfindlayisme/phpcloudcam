b450afd58e9435c2a910267e8b98fe2427be25c5
Chuck Findlay <chuck@findlayis.me>
2018-11-10 03:23:36 -0700

    SQL code to assign URL and ID to stream.php

--- www/stream.php
+++ www/stream.php
@@ -2,9 +2,19 @@
     // Copyright 2018 Chuck Findlay
     // This software is licensed under the GNU Lesser General Public License v3.0
 
-    // Include configuration
-    include('../config.inc.php');
-
     // No need for header or footer - we just output a stream. But users do need to be logged in
     include('../session.php');
+
+    // Given a camera ID to use
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $stmt = $db->prepare('SELECT url, content FROM stream_urls WHERE id = ?');
+        $stmt->bind_param('i',$id);
+
+        $stmt->execute();
+        $stmt->bind_result($url, $content);
+        $stmt->fetch();
+
+        // TO-DO: sending out the stream
+    }
 ?>

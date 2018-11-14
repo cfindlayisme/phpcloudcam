0d1df3b0eccc68fd12a567dddf2445d1789b00dc
Chuck Findlay <chuck@findlayis.me>
2018-11-14 15:25:20 -0700

    Addresses issue #12

--- theme/header.inc.php
+++ theme/header.inc.php
@@ -10,39 +10,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="stylesheet" href="<?php print BOOTSTRAP_CSS; ?>">
-  <style>
-    .row.content {
-        height: 700px;
-    }
-    
-    body {
-        height: 100%;
-        width: 100%;
-        position: absolute;
-    }
+  <link rel="stylesheet" href="css/main.css">
 
-    /* Set gray background color and 100% height */
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-    
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height: auto;} 
-    }
-  </style>
 </head>
 <body>
 <div class="container-fluid">

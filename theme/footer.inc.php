2fc60e5947bfb1d4c52c164d53eee40b9629514a
Chuck Findlay <chuck@findlayis.me>
2018-11-14 16:05:24 -0700

    Link to github on footer

--- theme/footer.inc.php
+++ theme/footer.inc.php
@@ -5,7 +5,7 @@
             </div>
         </div>
         <footer class="container-fluid">
-            <div id="licenseText">Produced by PHPCloudCam. Copyright 2018 Chuck Findlay. Released under the GNU Lesser General Public License v3.0</div>
+            <div id="licenseText">Produced by <a target="_blank" href="https://github.com/cfindlayisme/phpcloudcam">PHPCloudCam</a>. Copyright 2018 Chuck Findlay. Released under the GNU Lesser General Public License v3.0</div>
         </footer>
         <!-- Icons -->
         <script src="<?php print FEATHER_JS; ?>"></script>

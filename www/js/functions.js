0a8fa2706d3269c36bcf4546c24afa6bd82671f4
Chuck Findlay <chuck@findlayis.me>
2018-11-15 09:53:11 -0700

    Adjusted everything to use jquery

--- www/js/functions.js
+++ www/js/functions.js
@@ -3,54 +3,39 @@ function viewLiveView(cameraid) {
 }
 
 function viewRecording(id) {
-    document.getElementById('recordingsPlaybackSpace').innerHTML = '<video width="560" controls><source src="playback.php?id=' + id + '" type="video/mp4"></video>';
+    $('#recordingsPlaybackSpace').html('<video width="560" controls><source src="playback.php?id=' + id + '" type="video/mp4"></video>');
 }
 
 function viewSnapshot(cameraid) {
-    document.getElementById('snapshotSpace').innerHTML = '<img width="560" class="img-fluid img-rounded" src="snapshot.php?cameraid=' + cameraid + '"><br>';
+    $('#snapshotSpace').html('<img width="560" class="img-fluid img-rounded" src="snapshot.php?cameraid=' + cameraid + '"><br>');
 }
 
 function viewRecentRecordings() {
-    var xmlhttp, myObj, x, txt = '';
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            myObj = JSON.parse(this.responseText);
+    var txt = '';
+    $.getJSON('playback.php?getrecent=&limit=16', function(jd) {
             txt += '<select onchange="if (this.selectedIndex) viewRecording(this.selectedIndex);">';
             txt += '<option disabled selected value>--</option>';
-            for (x in myObj) {
-                txt += '<option value="' + myObj[x].id + '">' + myObj[x].timestamp + '</option>';
-            }
+            $.each(jd, function(key, val) {
+                txt += '<option value="' + val.id + '">' + val.timestamp + '</option>';
+            });
             txt += '</select><br><br>';
             txt += '<div id="recordingsPlaybackSpace"></div>';
-            document.getElementById('pageContent').innerHTML = txt;
-            document.getElementById('pageTitle').innerHTML = 'Recent Recordings';
-        }
-    }
-    xmlhttp.open('GET', 'playback.php?getrecent=&limit=16', true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send();
+            $('#pageContent').html(txt);
+            $('#pageTitle').html('Recent Recordings');
+    });
 }
 
-// List snapshot cameras on the sidebar
 function viewSnapshotsList() {
-    var xmlhttp, myObj, x, txt = '';
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            myObj = JSON.parse(this.responseText);
+    var txt = '';
+    $.getJSON('snapshot.php?labels=&limit=16', function(jd) {
             txt += '<select onchange="if (this.selectedIndex) viewSnapshot(this.selectedIndex);">';
             txt += '<option disabled selected value>--</option>';
-            for (x in myObj) {
-                txt += '<option value="' + myObj[x].cameraid + '">' + myObj[x].label + '</option>';
-            }
+            $.each(jd, function(key, val) {
+                txt += '<option value="' + val.cameraid + '">' + val.label + '</option>';
+            });
             txt += '</select><br><br>';
             txt += '<div id="snapshotSpace"></div>';
-            document.getElementById('pageContent').innerHTML = txt;
-            document.getElementById('pageTitle').innerHTML = 'Live Snapshots';
-        }
-    }
-    xmlhttp.open('GET', 'snapshot.php?labels=&limit=16', true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send();
+            $('#pageContent').html(txt);
+            $('#pageTitle').html('Live Snapshots');
+    });
 }

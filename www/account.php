125ce90e4865a6e8d98f964235d4fe501ed48146
Chuck Findlay <chuck@findlayis.me>
2018-11-15 11:24:40 -0700

    Account setting page password change UI mostly done (Issue #9)

---
+++ www/account.php
@@ -0,0 +1,33 @@
+<?php
+    // Copyright 2018 Chuck Findlay
+    // This software is licensed under the GNU Lesser General Public License v3.0
+
+    // Obviously user needs to be logged in
+    include('..' . DIRECTORY_SEPARATOR . 'session.php');
+
+    // From session.php we know $_SESSION['luser'] = the users ID index in SQL
+
+    if (isset($_GET['info'])) {
+        $stmt = $db->prepare('SELECT username FROM admin WHERE id = ?');
+
+        if ($stmt == false) {
+            // TO-DO: Output something to signify list is empty. For now just die
+            die('Nothing found');
+        }
+        $stmt->bind_param('i',$_SESSION['luser']);
+
+        $stmt->execute();
+        $stmt->bind_result($username);
+
+        $data = array();
+        while ( $stmt->fetch() ) {
+            $data[] = array('username' => $username);
+        }
+
+        $stmt->close();
+        print json_encode($data);
+    } else {
+        // Nothing useful was sent to us
+        http_response_code(400);
+    }
+?>
--- www/js/functions.js
+++ www/js/functions.js
@@ -8,8 +8,36 @@ function viewDashboard() {
 }
 
 function viewAccountSettings() {
-    $('#pageContent').html('');
-    $('#pageTitle').html('Account Settings');
+    var txt = '';
+    $.getJSON('account.php?info=', function(jd) {
+        txt += '<h4>Password Change</h4>';
+        txt += '<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">\
+                    <div class="form-group">\
+                        <label class="control-label required">Username:</label>\
+                        <input type="text" readonly class="form-control" name="username" value="'+jd[0].username+'">\
+                    </div>\
+                </div>';
+        txt += '<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">\
+                    <div class="form-group">\
+                        <label class="control-label required">Current password:</label>\
+                        <input type="password" type="text" class="form-control" name="cpassword">\
+                    </div>\
+                </div>';
+        txt += '<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">\
+                    <div class="form-group">\
+                        <label class="control-label required">New password:</label>\
+                        <input type="password" type="text" class="form-control" name="npassword1">\
+                    </div>\
+                </div>';
+        txt += '<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">\
+                    <div class="form-group">\
+                        <label class="control-label required">New password again:</label>\
+                        <input type="password" type="text" class="form-control" name="npassword2">\
+                    </div>\
+                </div>';
+        $('#pageContent').html(txt);
+        $('#pageTitle').html('Account Settings');
+    });
 }
 
 function viewRecording(id) {

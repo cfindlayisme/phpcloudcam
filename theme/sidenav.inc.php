f3cede954651783870ef61ddcb02fd5f8f3a1fc9
Chuck Findlay <chuck@findlayis.me>
2018-11-15 09:56:51 -0700

    Corrected an ID tag

--- theme/sidenav.inc.php
+++ theme/sidenav.inc.php
@@ -4,7 +4,7 @@
             <ul class="nav nav-pills nav-stacked">
             <li class="active" id="dashboardNav"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
             <li class="nav-item" id="snapshotNav"><a class="nav-link" onclick="viewSnapshotsList();" href="#">Live Snapshots</a></li>
-            <li class="nav-item" id="snapshotNav"><a class="nav-link" onclick="viewRecentRecordings();" href="#">Recent Recordings</a></li>
+            <li class="nav-item" id="recentRecordingsNav"><a class="nav-link" onclick="viewRecentRecordings();" href="#">Recent Recordings</a></li>
             </ul><br>
 
         </div>
